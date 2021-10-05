b27924319e397e9d492f16409fe1610f084a9a6a
Lucas Figueiredo <lucasborges_06@hotmail.com>
2021-10-05 13:56:44 -0300

    master

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                 switch (opcaoUsuario)
+                 {  
+                     case "1":
+                        //TODO: adicionar aluno
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        break;
+                    case "3":
+                        //TODO: calcular média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                 }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+
+            }
+        }
+
+
+        private static string ObterOpcaoUsuario(){
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair")
+            Console.WriteLine();
+
+            opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+        }
+    }
+}
