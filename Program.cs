748222b14c313c9f92c310709645d1c7e3edde28
Lucas Figueiredo <lucasborges_06@hotmail.com>
2021-10-05 18:10:25 -0300

    Ajuste Final

--- Program.cs
+++ Program.cs
@@ -6,20 +6,60 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
             string opcaoUsuario = ObterOpcaoUsuario();
 
-            while (opcaoUsuario.ToUpper() != "X");
+            while (opcaoUsuario.ToUpper() != "X")
             {
                  switch (opcaoUsuario)
                  {  
                      case "1":
-                        //TODO: adicionar aluno
+                        Console.WriteLine("Informe o Nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno: ");
+                        
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                                                
                         break;
                     case "2":
-                        //TODO: listar alunos
+                        foreach (var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                              Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");  
+                            }
+                            
+                        }
                         break;
                     case "3":
-                        //TODO: calcular média geral
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+
+                        for (int i=0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Console.WriteLine($"Média Geral: {mediaGeral}");
+
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
@@ -31,8 +71,10 @@ namespace Revisao
         }
 
 
-        private static string ObterOpcaoUsuario(){
-            Console.WriteLine("Informe a opção desejada");
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada: ");
             Console.WriteLine("1- Inserir novo aluno");
             Console.WriteLine("2- Listar alunos");
             Console.WriteLine("3- Calcular média geral");
@@ -40,6 +82,7 @@ namespace Revisao
             Console.WriteLine();
 
             string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
             return opcaoUsuario;
         }
     }
